84ffdc6fcf31c567b4eba8479ee1ad9fdbdf2a0b
周荣青 <754506559@qq.com>
2015-09-30 17:42:56 +0800

    删除启动面板

--- AccountBook/Main/ABAppDelegate.m
+++ AccountBook/Main/ABAppDelegate.m
@@ -18,9 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
     ABNavigationController *mainViewController = [[ABNavigationController alloc] initWithRootViewController:[[ABCategoryViewController alloc] init]];
     self.window.rootViewController = mainViewController;
-    self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
     return YES;
