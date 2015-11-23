ef7f44a291996dcc5bf93420e8d636a936f3b206
周荣青 <754506559@qq.com>
2015-11-23 10:57:15 +0800

    NSLog输出控制

--- AccountBook/Utils/ABDefine.h
+++ AccountBook/Utils/ABDefine.h
@@ -15,4 +15,19 @@
 ///是否iPad
 #define ABIsiPad [[UIDevice currentDevice] userInterfaceIdiom]
 
+#ifdef DEBUG
+#define LogPoint(p) NSLog(@"%s [Line %d]:%f,%f";__PRETTY_FUNCTION__, __LINE__,p.x, p.y)
+#define LogSize(p) NSLog(@"%s [Line %d]:%f,%f", __PRETTY_FUNCTION__, __LINE__,p.width, p.height)
+#define LogRect(p) NSLog(@"%s [Line %d]:%f,%f,%f,%f",__PRETTY_FUNCTION__, __LINE__,p.origin.x, p.origin.y, p.size.width, p.size.height)
+#define Log(fmt, ...) NSLog((@"%s [Line %d]: " fmt),__PRETTY_FUNCTION__, __LINE__,##__VA_ARGS__)
+#define LogObj(o) NSLog(@"%s [Line %d]:%@",__PRETTY_FUNCTION__, __LINE__,[o debugDescription])
+#else
+#define LogPoint(p)
+#define LogSize(p)
+#define LogRect(p)
+#define Log(fmt, ...)
+#define LogObj(o)
+#define NSLog(...)
+#endif
+
 #endif
